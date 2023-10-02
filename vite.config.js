ee871188301c8e9a97924e60196aab8954004204
Peter van Vliet <peter@masking.tech>
2023-10-02 12:16:55 +0200

    #1: Removed config file generated while setting up the linting process

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import jitar from '@jitar/plugin-vite';
-export default defineConfig({
-    plugins: [
-        react(),
-        jitar('src', 'shared', 'http://localhost:3000')
-    ]
-});
