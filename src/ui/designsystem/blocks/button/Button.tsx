fb22fa4ec7e80587220cb2ee267cbae372378d65
Bas Meeuwissen <bas@masking.tech>
2023-11-29 21:01:17 +0100

    First draft for buttons

--- src/ui/designsystem/blocks/button/Button.tsx
+++ src/ui/designsystem/blocks/button/Button.tsx
@@ -1,13 +1,17 @@
 
+import { MouseEventHandler } from 'react';
 import './Button.css';
 
-export type ButtonProps = {
-    type: "primary" | "secondary" | "disabled";
-};
+export type ButtonProps =
+    {
+        type: "primary" | "secondary" | "disabled";
+        text: string;
+        onclick?: MouseEventHandler<HTMLButtonElement>;
+    };
 
 export default function Button(props: ButtonProps)
 {
     return (
-        <button>Click me</button>
+        <button onClick={props.onclick} className={props.type} disabled={props.type === "disabled"}>{props.text}</button>
     );
 }
