d73a99b39ccf402576c3c5e4fa77e98b72bd4e7a
Bas Meeuwissen <bas@masking.tech>
2023-12-01 12:50:05 +0100

    Added design system strategy

--- src/ui/designsystem/blocks/panel/Panel.tsx
+++ src/ui/designsystem/blocks/panel/Panel.tsx
@@ -1,16 +1,15 @@
 
 import './Panel.css';
 
-export type PanelProps =
-    {
-        type: "normal" | "alert" | "card";
-        children: React.ReactNode;
-    };
+export type PanelProps = {
+    type: "normal" | "alert" | "card";
+    children?: React.ReactNode;
+};
 
 export default function Panel(props: PanelProps)
 {
     return (
-        <div className={props.type} >
+        <div className={"ds-panel ds-panel-" + props.type} >
             {props.children}
         </div>
     );
--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+import './designsystem/DesignSystem.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
