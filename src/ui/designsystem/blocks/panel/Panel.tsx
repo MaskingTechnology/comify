919127e6eee58d109b066fde90b888813e6bdd24
Bas Meeuwissen <bas@masking.tech>
2023-12-01 15:51:17 +0100

    #32: Implemented Panel

--- src/ui/designsystem/blocks/panel/Panel.tsx
+++ src/ui/designsystem/blocks/panel/Panel.tsx
@@ -2,14 +2,14 @@
 import './Panel.css';
 
 export type PanelProps = {
-    type: "normal" | "alert" | "card";
+    type: "normal" | "alert" | "border";
     children?: React.ReactNode;
 };
 
 export default function Panel(props: PanelProps)
 {
     return (
-        <div className={"ds-panel ds-panel-" + props.type} >
+        <div className={"ds-panel ds-panel-" + props.type}>
             {props.children}
         </div>
     );
