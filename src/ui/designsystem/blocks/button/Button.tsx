dc541b3636b37444295cb4840aea5aa2994843b4
Bas Meeuwissen <bas@masking.tech>
2023-12-01 15:51:30 +0100

    #32: Implemented button

--- src/ui/designsystem/blocks/button/Button.tsx
+++ src/ui/designsystem/blocks/button/Button.tsx
@@ -1,17 +1,19 @@
 
 import { MouseEventHandler } from 'react';
+
 import './Button.css';
 
-export type ButtonProps =
-    {
-        type: "primary" | "secondary" | "disabled";
-        text: string;
-        onclick?: MouseEventHandler<HTMLButtonElement>;
-    };
+export type ButtonProps = {
+    type: "primary" | "secondary" | "disabled";
+    text: string;
+    onclick?: MouseEventHandler<HTMLButtonElement>;
+};
 
 export default function Button(props: ButtonProps)
 {
     return (
-        <button onClick={props.onclick} className={props.type} disabled={props.type === "disabled"}>{props.text}</button>
+        <button onClick={props.onclick} className={"ds-button ds-button-" + props.type} disabled={props.type === "disabled"}>
+            {props.text}
+        </button>
     );
 }
