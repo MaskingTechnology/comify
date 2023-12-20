ee0e81a114addeddef7c0a718909965ccdfe1a3c
Bas Meeuwissen <bas@masking.tech>
2023-12-20 19:45:16 +0100

    #32: fixed ci and linting issues

--- src/webui/designsystem/components/selection/Selection.tsx
+++ src/webui/designsystem/components/selection/Selection.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import './Selection.css';
 
--- src/webui/main.tsx
+++ src/webui/main.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import './designsystem/DesignSystem.css';
+import './designsystem/designsystem.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
