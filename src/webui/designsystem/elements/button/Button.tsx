0aecab43102132e289a3d807bbc1c4aa8b3e7955
Bas Meeuwissen <bas@masking.tech>
2024-01-05 23:51:43 +0100

    #64: fixed issue found by Vite 5

--- src/webui/designsystem/elements/button/Button.tsx
+++ src/webui/designsystem/elements/button/Button.tsx
@@ -11,9 +11,10 @@ export type ButtonProps = {
 
 export default function Button(props: ButtonProps)
 {
-    const className = 'ds-button ds-button-' + props.type ?? 'primary';
+    const type = props.type ?? 'primary';
+    const className = 'ds-button ds-button-' + type;
     const disabled = props.type === 'disabled';
-    const type = props.type === 'submit' ? 'submit' : 'button';
+    const inputType = props.type === 'submit' ? 'submit' : 'button';
 
-    return <input type={type} onClick={props.clickHandler} className={className} disabled={disabled} value={props.text} />;
+    return <input type={inputType} onClick={props.clickHandler} className={className} disabled={disabled} value={props.text} />;
 }
