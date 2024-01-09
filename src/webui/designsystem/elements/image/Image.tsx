ef8c789b9455be32e416a4ef92ed6207dc765515
Bas Meeuwissen <bas@masking.tech>
2024-01-09 17:00:46 +0100

    #61 removed size as it is no logical property of an image

--- src/webui/designsystem/elements/image/Image.tsx
+++ src/webui/designsystem/elements/image/Image.tsx
@@ -4,20 +4,16 @@ import React from 'react';
 import './Image.css';
 
 export type ImageProps = {
-    size?: 'large' | 'medium' | 'small';
     source: string;
     title?: string;
     alt?: string;
-    width?: number;
-    height?: number;
+    width?: string;
+    height?: string;
 };
 
 export default function Image(props: ImageProps)
 {
-    const size = props.size || 'medium';
-
     return <img
-        className={'ds-image ds-image-size-' + size}
         title={props.title}
         alt={props.alt}
         width={props.width}
