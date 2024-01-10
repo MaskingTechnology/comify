3db85cee0d25faf61e6d6322b411eded0bc3b496
Peter van Vliet <peter@masking.tech>
2024-01-10 11:24:19 +0100

    #6: First draft of the authentication middleware for the gateway

--- src/integrations/jitar/setUpNode.ts
+++ src/integrations/jitar/setUpNode.ts
@@ -1,5 +1,13 @@
 
+import authentication from '../authentication/module';
 import fileStorage from '../filestorage/module';
+
+await authentication.connect({
+    issuer: 'http://localhost:8080/realms/comify',
+    clientId: 'openid',
+    clientSecret: '',
+    redirectUri: 'http://localhost:3000/rpc/domain/authentication/login'
+});
 
 await fileStorage.connect();
 
