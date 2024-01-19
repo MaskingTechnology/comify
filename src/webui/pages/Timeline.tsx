a03188cd9f98df9d2a7856870cbc79c49cc808d2
Peter van Vliet <peter@masking.tech>
2024-01-19 14:33:56 +0100

    #78: First filling of the timeline page

--- src/webui/pages/Timeline.tsx
+++ src/webui/pages/Timeline.tsx
@@ -3,9 +3,17 @@ import React from 'react';
 
 import Application from './templates/Application';
 
+import { OrderSelection, PostList } from '../components/module';
+
 export default function Timeline()
 {
+    const handleOrderChange = (oldKey: string, newKey: string) =>
+    {
+        console.log(`Order changed from ${oldKey} to ${newKey}`);
+    };
+
     return <Application>
-        Timeline
+        <OrderSelection selected='recent' changeHandler={handleOrderChange} />
+        <PostList />
     </Application>;
 }
