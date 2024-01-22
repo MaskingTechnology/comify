d9761dc78a3caf3f20143da32526d8d5915bf24f
Peter van Vliet <peter@masking.tech>
2024-01-22 21:07:37 +0100

    #78: Added missing features to the design system

--- src/webui/designsystem/components/tabs/Tabs.tsx
+++ src/webui/designsystem/components/tabs/Tabs.tsx
@@ -8,11 +8,22 @@ import './Tabs.css';
 export type TabsProps = {
     separator?: React.ReactNode;
     children: React.ReactElement<TabProps> | React.ReactElement<TabProps>[];
+    changeHandler?: (oldIndex: number, newIndex: number) => void;
 };
 
 export default function Tabs(props: TabsProps)
 {
     const [selected, setSelected] = useState(0);
+
+    const handleChange = (index: number) =>
+    {
+        if (props.changeHandler !== undefined)
+        {
+            props.changeHandler(selected, index);
+        }
+
+        setSelected(index);
+    };
 
     const children = Array.isArray(props.children)
         ? props.children
@@ -25,7 +36,7 @@ export default function Tabs(props: TabsProps)
                     children.map((element, index) =>
                     {
                         const style = index === selected ? 'active' : 'inactive';
-                        const handleClick = () => setSelected(index);
+                        const handleClick = () => handleChange(index);
 
                         return (
                             <div key={index} className={'ds-tabs-nav-item ' + style} onClick={handleClick}>
--- src/webui/designsystem/elements/row/Row.tsx
+++ src/webui/designsystem/elements/row/Row.tsx
@@ -7,6 +7,7 @@ export type RowProps = {
     align?: 'left' | 'center' | 'right' | 'justify';
     gap?: 'large' | 'medium' | 'small' | 'none';
     wrap?: 'wrap' | 'nowrap';
+    stretch?: boolean;
     children: React.ReactNode;
 };
 
@@ -15,8 +16,14 @@ export default function Row(props: RowProps)
     const align = props.align ?? 'left';
     const gap = props.gap ?? 'medium';
     const wrap = props.wrap ?? 'nowrap';
+    const stretch = props.stretch ?? false;
 
-    return <div className={'ds-row ds-row-align-' + align + ' ds-row-gap-' + gap + ' ds-row-wrap-' + wrap}>
+    const className = 'ds-row ds-row-align-' + align
+        + ' ds-row-gap-' + gap
+        + ' ds-row-wrap-' + wrap
+        + (stretch ? ' ds-row-stretch' : '');
+
+    return <div className={className}>
         {props.children}
     </div>;
 }
