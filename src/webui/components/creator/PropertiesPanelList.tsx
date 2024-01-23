78a2d4c0ff74d3bd1dffbb1b6405cecabf04e044
Peter van Vliet <peter@masking.tech>
2024-01-23 10:18:19 +0100

    #78: Put lists in a column to get a grip on the spacing between the components

--- src/webui/components/creator/PropertiesPanelList.tsx
+++ src/webui/components/creator/PropertiesPanelList.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 
 import type Creator from '../../../domain/client/views/Creator';
 
+import { Column } from '../../designsystem/module';
+
 import PropertiesPanel from './PropertiesPanel.js';
 
 export type Props = {
@@ -15,5 +17,7 @@ export default function Component(props: Props)
     const creators = props.creators;
     const followHandler = props.followHandler;
 
-    return creators.map(creator => <PropertiesPanel key={creator.id} creator={creator} followHandler={followHandler} />);
+    return <Column gap='medium'>
+        {creators.map(creator => <PropertiesPanel key={creator.id} creator={creator} followHandler={followHandler} />)}
+    </Column>;
 }
--- src/webui/components/post/LargePanelList.tsx
+++ src/webui/components/post/LargePanelList.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import type Creator from '../../../domain/client/views/Creator';
 import type Post from '../../../domain/client/views/Post';
 
+import { Column } from '../../designsystem/module';
+
 import LargePanel from './LargePanel';
 
 export type Props = {
@@ -16,5 +18,7 @@ export default function Component(props: Props)
     const posts = props.posts;
     const followHandler = props.followHandler;
 
-    return posts.map(post => <LargePanel key={post.id} post={post} followHandler={followHandler} />);
+    return <Column gap='medium'>
+        {posts.map(post => <LargePanel key={post.id} post={post} followHandler={followHandler} />)}
+    </Column>;
 }
