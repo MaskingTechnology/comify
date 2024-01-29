83d45cb7221470b2277f410c2fc531f3dc2feb86
Peter van Vliet <peter@masking.tech>
2024-01-29 11:24:23 +0100

    #89: Added navigation icons

--- src/webui/components/application/navigation/Item.tsx
+++ src/webui/components/application/navigation/Item.tsx
@@ -1,22 +1,27 @@
 
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 import { Row, Text, Image } from '../../../designsystem/module';
 
 import Link from '../link/Link';
 
 export type Props = {
-    icon: string;
     title: string;
     to: string;
+    activeIcon: string;
+    inactiveIcon: string;
 };
 
-export default function Component({ icon, title, to }: Props)
+export default function Component({ title, to, activeIcon, inactiveIcon }: Props)
 {
+    const location = useLocation();
+    const isActive = location.pathname === to;
+
     return <Link to={to}>
         <Row gap='medium' alignY='center'>
-            <Image source={icon} width='13px' />
-            <Text value={title} size='large' weight='bold' />
+            <Image source={isActive ? activeIcon : inactiveIcon} width='25em' />
+            <Text value={title} size='large' weight={isActive ? 'bold' : 'normal'} />
         </Row>
     </Link>;
 }
--- src/webui/components/application/navigation/Navigation.tsx
+++ src/webui/components/application/navigation/Navigation.tsx
@@ -3,17 +3,28 @@ import React from 'react';
 
 import { Column } from '../../../designsystem/module';
 
-import { Props as ItemProps } from './Item';
+import Item from './Item';
 
-export type Props = {
-    children: React.ReactElement<ItemProps> | React.ReactElement<ItemProps>[];
-};
+import timelineActiveIcon from '../../../../assets/images/navigation/timeline-active.svg';
+import timelineInactiveIcon from '../../../../assets/images/navigation/timeline-inactive.svg';
+import exploreActiveIcon from '../../../../assets/images/navigation/explore-active.svg';
+import exploreInactiveIcon from '../../../../assets/images/navigation/explore-inactive.svg';
+import notificationsActiveIcon from '../../../../assets/images/navigation/notifications-active.svg';
+import notificationsInactiveIcon from '../../../../assets/images/navigation/notifications-inactive.svg';
+import createActiveIcon from '../../../../assets/images/navigation/create-active.svg';
+import createInactiveIcon from '../../../../assets/images/navigation/create-inactive.svg';
+import profileActiveIcon from '../../../../assets/images/navigation/profile-active.svg';
+import profileInactiveIcon from '../../../../assets/images/navigation/profile-inactive.svg';
 
-export default function Component({ children }: Props)
+export default function Component()
 {
     return <nav>
         <Column gap='medium'>
-            {children}
+            <Item title='Timeline' to='/timeline' activeIcon={timelineActiveIcon} inactiveIcon={timelineInactiveIcon} />
+            <Item title='Explore' to='/explore' activeIcon={exploreActiveIcon} inactiveIcon={exploreInactiveIcon} />
+            <Item title='Notifications' to='/notifications' activeIcon={notificationsActiveIcon} inactiveIcon={notificationsInactiveIcon} />
+            <Item title='Create' to='/create' activeIcon={createActiveIcon} inactiveIcon={createInactiveIcon} />
+            <Item title='Profile' to='/profile' activeIcon={profileActiveIcon} inactiveIcon={profileInactiveIcon} />
         </Column>
     </nav>;
 }
