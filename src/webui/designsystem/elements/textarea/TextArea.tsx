21e053a5514686390b5623d8569ff6fcaa087500
Peter van Vliet <peter@masking.tech>
2024-01-29 14:16:00 +0100

    #89: Added sizing options for design system input elements

--- src/webui/designsystem/elements/textarea/TextArea.tsx
+++ src/webui/designsystem/elements/textarea/TextArea.tsx
@@ -5,13 +5,14 @@ import './TextArea.css';
 
 export type Props = {
     name: string;
-    size?: 'large' | 'medium' | 'small';
     placeholder?: string;
     value?: string;
+    size?: 'large' | 'medium' | 'small';
+    rows?: number;
     changeHandler?: React.ChangeEventHandler<HTMLTextAreaElement>;
 };
 
-export default function Element({ name, size, placeholder, value, changeHandler }: Props)
+export default function Element({ name, placeholder, value, size, rows, changeHandler }: Props)
 {
     const className = 'ds-textarea'
         + ' ds-textarea-size-' + (size ?? 'medium');
@@ -21,6 +22,7 @@ export default function Element({ name, size, placeholder, value, changeHandler 
         name={name}
         placeholder={placeholder}
         defaultValue={value}
+        rows={rows}
         onChange={changeHandler}>
     </textarea>;
 }
--- src/webui/designsystem/elements/textbox/TextBox.tsx
+++ src/webui/designsystem/elements/textbox/TextBox.tsx
@@ -7,13 +7,17 @@ export type Props = {
     name: string;
     placeholder?: string;
     value?: string;
+    size?: 'large' | 'medium' | 'small';
     changeHandler?: React.ChangeEventHandler<HTMLInputElement>;
 };
 
-export default function Element({ name, placeholder, value, changeHandler }: Props)
+export default function Element({ name, placeholder, value, size, changeHandler }: Props)
 {
+    const className = 'ds-textbox'
+        + ' ds-textbox-size-' + (size ?? 'medium');
+
     return <input
-        className='ds-textbox'
+        className={className}
         type='text'
         name={name}
         placeholder={placeholder}
