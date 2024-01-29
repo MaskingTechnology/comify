cbd1270f8fcc98447dd229d2d9eda7a50af842a2
Peter van Vliet <peter@masking.tech>
2024-01-29 12:23:40 +0100

    #89: Added engagement icons

--- src/webui/components/post/elementary/EngagementRow.tsx
+++ src/webui/components/post/elementary/EngagementRow.tsx
@@ -1,26 +1,31 @@
 
 import React from 'react';
 
-import { Icon, Row } from '../../../designsystem/module';
+import { Image, Row } from '../../../designsystem/module';
 
 import CompactNumber from '../../common/CompactNumber';
 
+import unratedIcon from '../../../../assets/images/icons/unrated.svg';
+import ratedIcon from '../../../../assets/images/icons/rated.svg';
+import reactionsIcon from '../../../../assets/images/icons/reactions.svg';
+
 export type Props = {
+    isRated: boolean;
     ratingCount: number;
     reactionCount: number;
     rateHandler?: () => void;
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function Component({ ratingCount, reactionCount, rateHandler }: Props)
+export default function Component({ isRated, ratingCount, reactionCount, rateHandler }: Props)
 {
     return <Row gap='medium' alignY='center'>
         <Row gap='small' alignY='center'>
-            <Icon type='heart' />
+            <Image source={isRated ? ratedIcon : unratedIcon} height='16px' />
             <CompactNumber value={ratingCount} />
         </Row>
         <Row gap='small' alignY='center'>
-            <Icon type='comment' />
+            <Image source={reactionsIcon} height='16px' />
             <CompactNumber value={reactionCount} />
         </Row>
     </Row>;
