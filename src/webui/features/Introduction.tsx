517595d4154aa1952b44ea2f911df733222d255f
Peter van Vliet <peter@masking.tech>
2024-02-02 13:35:05 +0100

    #91: implemented the 'Get in' button

--- src/webui/features/Introduction.tsx
+++ src/webui/features/Introduction.tsx
@@ -1,6 +1,8 @@
 
 import React from 'react';
 
+import getLoginUrl from '../../domain/authentication/getLoginUrl';
+
 import { Button, Column, Image, Panel, Row, Text } from '../designsystem/module';
 
 import { Logo } from '../components/module';
@@ -9,11 +11,18 @@ import introductionImage from '../../assets/images/introduction.png';
 
 export default function Feature()
 {
+    async function getIn()
+    {
+        const loginUrl = await getLoginUrl();
+
+        location.href = loginUrl;
+    }
+
     return <Panel>
         <Column gap='medium' alignX='stretch'>
             <Row alignX='justify' alignY='top'>
                 <Logo />
-                <Button type='primary' text='Get in' />
+                <Button type='primary' text='Get in' clickHandler={() => getIn()} />
             </Row>
             <Image source={introductionImage} width='600px' />
             <Text size='large' value='Take or upload a picture. Add speech bubbles. Share with friends.' />
