49cd909771ea6e639b4c8988b3a6a6d70a7c1044
Peter van Vliet <peter@masking.tech>
2024-02-03 01:20:01 +0100

    #91: added redirect to the timeline page after login

--- src/webui/features/Login.tsx
+++ src/webui/features/Login.tsx
@@ -1,17 +1,20 @@
 
 import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import Requester from '../../domain/authentication/Requester';
 import getMe from '../../domain/creator/getMe';
 
 export default function Login()
 {
+    const navigate = useNavigate();
+
     const logMeIn = async () =>
     {
         const placeholder = new Requester('placeholder', 'placeholder', 'placeholder');
         const me = await getMe(placeholder);
 
-        console.log('logMeIn', me.fullName);
+        navigate('/timeline');
     };
 
     useEffect(() =>
