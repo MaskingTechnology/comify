01a68148ea577f7c33e045ded60d03a6f52ad7e9
Peter van Vliet <peter@masking.tech>
2024-02-09 17:17:48 +0100

    #111: Added and configured the app icon

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,22 @@ import jitar from '@jitar/plugin-vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+const JITAR_URL = 'http://localhost:3000';
+const JITAR_SEGMENTS = [];
+const JITAR_MIDDLEWARES = ['./integrations/jitar/requesterMiddleware'];
+
 export default defineConfig({
   publicDir: 'src/webui/public',
   build: {
     assetsDir: 'webui'
   },
+  server: {
+    proxy: {
+      '/assets': JITAR_URL
+    }
+  },
   plugins: [
     react(),
-    jitar('src', 'domain', 'http://localhost:3000', [], ['./integrations/jitar/requesterMiddleware'])
+    jitar('src', 'domain', JITAR_URL, JITAR_SEGMENTS, JITAR_MIDDLEWARES)
   ]
 });
