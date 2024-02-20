3241561d296574b9403e61cd7a9691c07b3ff690
Peter van Vliet <peter@masking.tech>
2024-02-20 10:53:21 +0100

    #133: stabilized image selection

--- src/webui/editor/utils/FileDialog.ts
+++ src/webui/editor/utils/FileDialog.ts
@@ -1,26 +1,33 @@
 
-const input = document.createElement('input');
-input.type = 'file';
-input.accept = 'image/*';
-
 export default class FileDialog
 {
     static open(): Promise<File | undefined>
     {
-        return new Promise((resolve) =>
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = 'image/*';
+        input.multiple = false;
+
+        return new Promise((resolve, reject) =>
         {
             input.onchange = () =>
             {
-                const file = this.#getSelectedFile();
+                const file = this.#getSelectedFile(input);
 
                 resolve(file);
             };
+
+            input.oncancel = () => { console.log('cancel'); resolve(undefined); };
+            input.onabort = () => { console.log('abort'); resolve(undefined); };
+
+            input.oninvalid = () => { reject(undefined); };
+            input.onerror = () => { reject(undefined); };
 
             input.click();
         });
     }
 
-    static #getSelectedFile(): File | undefined
+    static #getSelectedFile(input: HTMLInputElement): File | undefined
     {
         const files = input.files;
 
