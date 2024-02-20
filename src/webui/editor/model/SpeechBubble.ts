20c985c407e67bd75a0464761e27e9098b6dcb1b
Peter van Vliet <peter@masking.tech>
2024-02-20 03:52:37 +0100

    #133: optimizations

--- src/webui/editor/model/SpeechBubble.ts
+++ src/webui/editor/model/SpeechBubble.ts
@@ -4,14 +4,15 @@ import Geometry, { Area, Point } from '../utils/Geometry';
 import Bubble from './Bubble';
 
 type Shape = {
-    area: Area;
+    balloon: Area;
     baseLeft: Point;
     baseRight: Point;
     pointer: Point;
 };
 
 const BUBBLE_RADIUS = 10;
-const BUBBLE_POINTER_RATIO = 0.3;
+const POINTER_RATIO = 0.3;
+const SHADOW_OFFSET = 3;
 
 export default class SpeechBubble extends Bubble
 {
@@ -26,38 +27,49 @@ export default class SpeechBubble extends Bubble
 
     #createShape(): Shape
     {
-        const area = this.area;
+        const balloon = {
+            x: Math.round(this.area.x),
+            y: Math.round(this.area.y),
+            width: Math.round(this.area.width),
+            height: Math.round(this.area.height),
+        };
+
         const center = this.center;
         const angle = Geometry.calculateAngle(center, this.pointer);
 
         const pointer = this.pointer;
-        const bubbleSize = (area.width + area.height) / 2;
+        const bubbleSize = (balloon.width + balloon.height) / 2;
 
-        const baseSize = Math.round(bubbleSize * BUBBLE_POINTER_RATIO);
+        const baseSize = Math.round(bubbleSize * POINTER_RATIO);
         const baseOffset = Math.round(baseSize / 2);
         const baseLeft = Geometry.rotateAndTranslatePoint({ x: -baseOffset, y: 0 }, center, angle);
         const baseRight = Geometry.rotateAndTranslatePoint({ x: baseOffset, y: 0 }, center, angle);
 
-        return { area, baseLeft, baseRight, pointer };
+        baseLeft.x = Math.round(baseLeft.x);
+        baseLeft.y = Math.round(baseLeft.y);
+        baseRight.x = Math.round(baseRight.x);
+        baseRight.y = Math.round(baseRight.y);
+
+        return { balloon, baseLeft, baseRight, pointer };
     }
 
     #createShadowShape(shape: Shape): Shape
     {
-        const area = { ...shape.area };
-        area.x += 5;
-        area.y += 5;
+        const balloon = { ...shape.balloon };
+        balloon.x += SHADOW_OFFSET;
+        balloon.y += SHADOW_OFFSET;
 
         const baseLeft = { ...shape.baseLeft };
-        baseLeft.x += 5;
-        baseLeft.y += 5;
+        baseLeft.x += SHADOW_OFFSET;
+        baseLeft.y += SHADOW_OFFSET;
 
         const baseRight = { ...shape.baseRight };
-        baseRight.x += 5;
-        baseRight.y += 5;
+        baseRight.x += SHADOW_OFFSET;
+        baseRight.y += SHADOW_OFFSET;
 
         const pointer = { ...shape.pointer };
 
-        return { area, baseLeft, baseRight, pointer };
+        return { balloon, baseLeft, baseRight, pointer };
     }
 
     #renderShape(shape: Shape, color: string, context: CanvasRenderingContext2D): void
@@ -65,10 +77,19 @@ export default class SpeechBubble extends Bubble
         context.fillStyle = color;
 
         context.beginPath();
-        context.roundRect(shape.area.x, shape.area.y, shape.area.width, shape.area.height, BUBBLE_RADIUS);
-        context.moveTo(Math.round(shape.baseLeft.x), Math.round(shape.baseLeft.y));
-        context.lineTo(Math.round(shape.baseRight.x), Math.round(shape.baseRight.y));
-        context.lineTo(Math.round(shape.pointer.x), Math.round(shape.pointer.y));
+
+        context.roundRect(
+            shape.balloon.x,
+            shape.balloon.y,
+            shape.balloon.width,
+            shape.balloon.height,
+            BUBBLE_RADIUS
+        );
+
+        context.moveTo(shape.baseLeft.x, shape.baseLeft.y);
+        context.lineTo(shape.baseRight.x, shape.baseRight.y);
+        context.lineTo(shape.pointer.x, shape.pointer.y);
+
         context.closePath();
         context.fill();
     }
