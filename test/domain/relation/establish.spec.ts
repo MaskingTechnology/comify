967a4ef49204eae0add5b8f60f0944a0593ae9f9
JohnAtmasking <john@masking.tech>
2024-03-07 13:36:55 +0100

    #134: split up tests on "root" level functions

--- test/domain/relation/establish.spec.ts
+++ test/domain/relation/establish.spec.ts
@@ -1,6 +1,6 @@
 
 import { describe, expect, it } from 'vitest';
-import { CREATOR0, CREATOR1, CREATOR2, REQUESTER1, REQUESTER2, RelationAlreadyExists, establish, getFollowers, getFollowing } from './_fixtures/relation.fixture';
+import { CREATOR0, CREATOR1, REQUESTER1, REQUESTER2, RelationAlreadyExists, establish } from './_fixtures/relation.fixture';
 
 describe('domain/relation/establish', () =>
 {
@@ -16,23 +16,5 @@ describe('domain/relation/establish', () =>
         const promise = establish(REQUESTER1, CREATOR1);
 
         expect(promise).rejects.toStrictEqual(new RelationAlreadyExists);
-    });
-
-    it('should retrieve relations for a follower', async () =>
-    {
-        const relations = await getFollowing(CREATOR0);
-
-        expect(relations.length).toBe(2);
-        expect(relations[0].following?.id).toBe(CREATOR1);
-        expect(relations[1].following?.id).toBe(CREATOR2);
-    });
-
-    it('should retrieve relations for a followee', async () =>
-    {
-        const relations = await getFollowers(CREATOR2);
-
-        expect(relations.length).toBe(2);
-        expect(relations[0].follower?.id).toBe(CREATOR0);
-        expect(relations[1].follower?.id).toBe(CREATOR1);
     });
 });
