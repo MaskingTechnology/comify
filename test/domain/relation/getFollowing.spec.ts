39f757e975e192d9073d9ac04e9eff957867da58
JohnAtmasking <john@masking.tech>
2024-04-13 23:49:45 +0200

    #154 processed more review comments

--- test/domain/relation/getFollowing.spec.ts
+++ test/domain/relation/getFollowing.spec.ts
@@ -1,15 +1,15 @@
 
 import { describe, expect, it } from 'vitest';
-import { CREATOR0_ID, CREATOR1_ID, CREATOR2_ID, REQUESTER1, getFollowers } from './_fixtures/relation.fixture';
+import { CREATOR0_ID, CREATOR1_ID, CREATOR2_ID, REQUESTER1, getFollowing } from './_fixtures/relation.fixture';
 
 describe('domain/relation/getFollowing', () =>
 {
-    it('should retrieve relations for a followee', async () =>
+    it('should retrieve relations for a follower', async () =>
     {
-        const relations = await getFollowers(REQUESTER1, CREATOR2_ID);
+        const relations = await getFollowing(REQUESTER1, CREATOR0_ID);
 
         expect(relations.length).toBe(2);
-        expect(relations[0].following?.id).toBe(CREATOR0_ID);
-        expect(relations[1].following?.id).toBe(CREATOR1_ID);
+        expect(relations[0].following?.id).toBe(CREATOR1_ID);
+        expect(relations[1].following?.id).toBe(CREATOR2_ID);
     });
 });
