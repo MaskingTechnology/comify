66107fe625b98635717b1124c75bbd89fa054e5c
Bas Meeuwissen <bas@masking.tech>
2024-04-14 07:54:45 +0200

    #157: typo in name

--- src/domain/reaction/data/retrieveByPost.ts
+++ src/domain/reaction/data/retrieveByPost.ts
@@ -4,7 +4,7 @@ import { RECORD_TYPE } from '../definitions/constants';
 import type ReactionData from './ReactionData';
 import createData from './mapRecord';
 
-export default async function retrieveByCreators(postId: string): Promise<ReactionData[]>
+export default async function retrieveByPost(postId: string): Promise<ReactionData[]>
 {
     const query: RecordQuery = { postId: { 'EQUALS': postId } };
     const sort: RecordSort = { createdAt: SortDirections.ASCENDING };
