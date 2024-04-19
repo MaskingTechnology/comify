9041b2dab631102aa44ba8d7cf3f636066084c4f
Bas Meeuwissen <bas@masking.tech>
2024-04-19 08:13:38 +0200

    #157: update post as last step in case undo would fail

--- src/domain/reaction/create.ts
+++ src/domain/reaction/create.ts
@@ -2,26 +2,26 @@
 import Requester from '../authentication/Requester';
 import createComment from '../comment/create';
 import removeComment from '../comment/remove';
-import decreaseReactionCount from '../post/decreaseReactionCount';
 import increaseReactionCount from '../post/increaseReactionCount';
 import createReaction from './data/create';
+import removeReaction from './data/remove';
 
 export default async function create(requester: Requester, postId: string, message: string): Promise<void>
 {
-    let comment, post;
+    let comment, reaction;
 
     try
     {
         comment = await createComment(message);
-        post = await increaseReactionCount(postId);
-        await createReaction(requester.id, postId, undefined, comment.id);
+        reaction = await createReaction(requester.id, postId, undefined, comment.id);
+        await increaseReactionCount(postId);
     }
     catch (error: unknown)
     {
         const undoComment = comment !== undefined ? removeComment(comment.id) : Promise.resolve();
-        const undoPost = post !== undefined ? decreaseReactionCount(postId) : Promise.resolve();
+        const undoReaction = reaction !== undefined ? removeReaction(reaction.id) : Promise.resolve();
 
-        await Promise.all([undoComment, undoPost]);
+        await Promise.all([undoComment, undoReaction]);
 
         throw error;
     }
