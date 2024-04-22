bf4e8ce213ee5027f9afad8439c11087e7c33459
Bas Meeuwissen <bas@masking.tech>
2024-04-22 16:28:51 +0200

    #155: retrieve posts from database

--- src/domain/post/data/retrieveWithoutCreators.ts
+++ src/domain/post/data/retrieveWithoutCreators.ts
@@ -1,8 +1,14 @@
 
+import database, { RecordQuery } from '../../../integrations/database/module';
+import { RECORD_TYPE } from '../definitions/constants';
+import mapRecord from './mapRecord';
 import type PostData from './PostData';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default async function retrieveWithoutCreators(creatorIds: string[]): Promise<PostData[]>
 {
-    return [];
+    const query: RecordQuery = { creatorId: { NOT_IN: creatorIds } };
+
+    const records = await database.searchRecords(RECORD_TYPE, query);
+
+    return Promise.all(records.map(mapRecord));
 }
--- src/domain/post/explore.ts
+++ src/domain/post/explore.ts
@@ -1,17 +1,17 @@
 
 import type Requester from '../authentication/Requester';
 import retrieveRelationsByFollower from '../relation/data/retrieveByFollower';
+import retrieveWithoutCreators from './data/retrieveWithoutCreators';
 import type PostView from './view/PostView';
 import createView from './view/createView';
-
-import retrieveWithoutCreators from './data/retrieveWithoutCreators';
 
 export default async function explore(requester: Requester): Promise<PostView[]>
 {
     const relationsData = await retrieveRelationsByFollower(requester.id);
-    const followingIds = relationsData.map(data => data.followingId);
-    const postData = await retrieveWithoutCreators(followingIds);
+    const excludedIds = relationsData.map(data => data.followingId);
+    excludedIds.push(requester.id);
 
+    const postData = await retrieveWithoutCreators(excludedIds);
     const views = postData.map(item => createView(requester, item));
 
     return Promise.all(views);
