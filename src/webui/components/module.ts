0da3c76058648e4b25da8081c662a9729bf8f8dc
Peter van Vliet <peter@masking.tech>
2024-04-25 15:20:32 +0200

    #192: implemented modal use

--- src/webui/components/module.ts
+++ src/webui/components/module.ts
@@ -4,6 +4,7 @@ export { default as ApplicationLegalInfo } from './application/LegalInfo';
 export { default as ApplicationLogo } from './application/Logo';
 export { default as ApplicationSidebar } from './application/sidebar/Sidebar';
 export { default as ComicEditor } from './comic/Editor';
+export { default as CommentForm } from './comment/Form';
 export { default as ErrorBoundary } from './common/ErrorBoundary';
 export { default as LoadingContainer } from './common/LoadingContainer';
 export { default as LogoutPanel } from './common/LogoutPanel';
---
+++ src/webui/features/CreateReaction.tsx
@@ -0,0 +1,31 @@
+
+import { ComicEditor, CommentForm } from '^/webui/components/module';
+import { Ruler, Tab, Tabs } from '^/webui/designsystem/module';
+
+type Props = {
+    handleDone: () => void;
+};
+
+export default function Feature({ handleDone }: Props)
+{
+    const createComic = async (imageData: string) =>
+    {
+        console.log('Create comic');
+        handleDone();
+    };
+
+    const createComment = async (commentText: string) =>
+    {
+        console.log('Create comment');
+        handleDone();
+    };
+
+    return <Tabs separator={<Ruler type='horizontal' size='small' />}>
+        <Tab title='Comic'>
+            <ComicEditor createHandler={createComic} />
+        </Tab>
+        <Tab title='Comment'>
+            <CommentForm createHandler={createComment} />
+        </Tab>
+    </Tabs>;
+}
