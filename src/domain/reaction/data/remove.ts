cd47964a1c5e9b4985f3ef3678ca7978dfe34c8e
Bas Meeuwissen <bas@masking.tech>
2024-05-13 14:45:04 +0200

    #222: forgotten feedback item

--- src/domain/reaction/data/remove.ts
+++ src/domain/reaction/data/remove.ts
@@ -1,12 +1,12 @@
 
-import database from '^/integrations/database/module';
+import database, { RecordData } from '^/integrations/database/module';
 
 import { RECORD_TYPE } from '../definitions/constants';
 import type ReactionData from './ReactionData';
 
 export default async function remove(post: ReactionData): Promise<void>
 {
-    const updatedData = { ...post, deleted: true };
+    const updatedData: RecordData = { ...post, deleted: true };
 
     return database.updateRecord(RECORD_TYPE, post.id, updatedData);
 }
