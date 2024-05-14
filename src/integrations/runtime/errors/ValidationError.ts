6b88745103065f2554cc4d1cc1414d15d23f02a1
Bas Meeuwissen <bas@masking.tech>
2024-05-14 21:03:10 +0200

    #148: extend jitar error into our own version

---
+++ src/integrations/runtime/errors/ValidationError.ts
@@ -0,0 +1,18 @@
+
+import { BadRequest } from 'jitar';
+
+export default class ValidationError extends BadRequest
+{
+    #messages: Map<string, string>;
+
+    constructor(messages: Map<string, string>)
+    {
+        const keys = [...messages.keys()].join(', ');
+
+        super(`Invalid field(s): ${keys}`);
+
+        this.#messages = messages;
+    }
+
+    get messages() { return this.#messages; }
+}
