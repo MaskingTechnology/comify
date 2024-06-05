f94ab6d720c652230e9f7f968555bccd62cc6b24
Bas Meeuwissen <bas@masking.tech>
2024-06-05 15:43:35 +0200

    #235: validate comics

--- src/domain/comic/create/feature.ts
+++ src/domain/comic/create/feature.ts
@@ -6,6 +6,7 @@ import { IMAGE_TYPE } from '../definitions';
 
 import createData from './createData';
 import insert from './insertData';
+import validateData from './validateData';
 
 export default async function feature(imageDataUrl: string, structure?: string): Promise<string>
 {
@@ -16,6 +17,8 @@ export default async function feature(imageDataUrl: string, structure?: string):
         imageId = await createImage(IMAGE_TYPE, imageDataUrl);
 
         const data = createData(imageId, structure);
+
+        validateData(data);
 
         return insert(data);
     }
--- src/domain/definitions.ts
+++ src/domain/definitions.ts
@@ -1,8 +1,30 @@
 
+import type { Validation } from '^/integrations/validation/module';
+
 export const SortOrder = {
     POPULAR: 'popular',
     RECENT: 'recent'
 } as const;
+
+export const requiredIdValidation: Validation =
+{
+    message: 'Invalid id format',
+    STRING:
+    {
+        required: true,
+        maxLength: 36
+    }
+};
+
+export const optionalIdValidation: Validation =
+{
+    message: 'Invalid id format',
+    STRING:
+    {
+        required: false,
+        maxLength: 36
+    }
+};
 
 type SortOrderKeys = keyof typeof SortOrder;
 
