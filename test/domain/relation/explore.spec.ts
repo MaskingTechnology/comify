4ec9af7a65268f296eb5f8653d11c0bd9697dadc
Bas Meeuwissen <bas@masking.tech>
2024-06-16 08:00:29 +0200

    #178: fixed tests

--- test/domain/relation/explore.spec.ts
+++ test/domain/relation/explore.spec.ts
@@ -15,7 +15,7 @@ describe('domain/relation/explore', () =>
 {
     it('should explore relations based on popularity', async () =>
     {
-        const relations = await explore(REQUESTERS.FIRST, SortOrder.POPULAR);
+        const relations = await explore(REQUESTERS.FIRST, SortOrder.POPULAR, undefined, VALUES.RANGE);
         expect(relations).toHaveLength(3);
         expect(relations[0].following?.id).toBe(VALUES.IDS.CREATOR5);
         expect(relations[1].following?.id).toBe(VALUES.IDS.CREATOR4);
@@ -24,7 +24,7 @@ describe('domain/relation/explore', () =>
 
     it('should explore relations based on recent', async () =>
     {
-        const relations = await explore(REQUESTERS.FIRST, SortOrder.RECENT);
+        const relations = await explore(REQUESTERS.FIRST, SortOrder.RECENT, undefined, VALUES.RANGE);
         expect(relations).toHaveLength(3);
         expect(relations[0].following?.id).toBe(VALUES.IDS.CREATOR4);
         expect(relations[1].following?.id).toBe(VALUES.IDS.CREATOR6);
@@ -33,27 +33,27 @@ describe('domain/relation/explore', () =>
 
     it('should find no relations based on search', async () =>
     {
-        const relations = await explore(REQUESTERS.FIRST, SortOrder.POPULAR, 'or2');
+        const relations = await explore(REQUESTERS.FIRST, SortOrder.POPULAR, 'or2', VALUES.RANGE);
         expect(relations).toHaveLength(0);
     });
 
     it('should find relations based on search full name', async () =>
     {
-        const relations = await explore(REQUESTERS.FIRST, SortOrder.POPULAR, 'or 4');
+        const relations = await explore(REQUESTERS.FIRST, SortOrder.POPULAR, 'or 4', VALUES.RANGE);
         expect(relations).toHaveLength(1);
         expect(relations[0].following?.id).toBe(VALUES.IDS.CREATOR4);
     });
 
     it('should find relations based on search nickname', async () =>
     {
-        const relations = await explore(REQUESTERS.FIRST, SortOrder.POPULAR, 'creator4');
+        const relations = await explore(REQUESTERS.FIRST, SortOrder.POPULAR, 'creator4', VALUES.RANGE);
         expect(relations).toHaveLength(1);
         expect(relations[0].following?.id).toBe(VALUES.IDS.CREATOR4);
     });
 
     it('should find relations based on search full name and nickname', async () =>
     {
-        const relations = await explore(REQUESTERS.FIRST, SortOrder.POPULAR, 'five');
+        const relations = await explore(REQUESTERS.FIRST, SortOrder.POPULAR, 'five', VALUES.RANGE);
         expect(relations).toHaveLength(2);
         expect(relations[0].following?.id).toBe(VALUES.IDS.CREATOR5);
         expect(relations[1].following?.id).toBe(VALUES.IDS.CREATOR6);
--- test/domain/relation/getFollowing.spec.ts
+++ test/domain/relation/getFollowing.spec.ts
@@ -14,7 +14,7 @@ describe('domain/relation/getFollowing', () =>
 {
     it('should retrieve relations for a follower', async () =>
     {
-        const relations = await getFollowing(REQUESTERS.FIRST, VALUES.IDS.CREATOR1);
+        const relations = await getFollowing(REQUESTERS.FIRST, VALUES.IDS.CREATOR1, VALUES.RANGE);
         expect(relations).toHaveLength(2);
         expect(relations[0].following?.id).toBe(VALUES.IDS.CREATOR2);
         expect(relations[1].following?.id).toBe(VALUES.IDS.CREATOR3);
