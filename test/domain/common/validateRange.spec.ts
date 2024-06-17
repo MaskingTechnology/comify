d5a97b28865e044f7e1f921e2300e82e5ceef894
Bas Meeuwissen <bas@masking.tech>
2024-06-17 16:29:35 +0200

    #178: execution seems to be a better name

--- test/domain/common/validateRange.spec.ts
+++ test/domain/common/validateRange.spec.ts
@@ -10,23 +10,23 @@ describe('domain/common/validateRange', () =>
 {
     it('should not accept a too small limit', () =>
     {
-        const result = () => validateRange(VALUES.RANGE.LIMIT_TOO_SMALL);
+        const execution = () => validateRange(VALUES.RANGE.LIMIT_TOO_SMALL);
 
-        expect(result).toThrow(InvalidRange);
+        expect(execution).toThrow(InvalidRange);
     });
 
     it('should not accept a too big limit', () =>
     {
-        const result = () => validateRange(VALUES.RANGE.LIMIT_TOO_BIG);
+        const execution = () => validateRange(VALUES.RANGE.LIMIT_TOO_BIG);
 
-        expect(result).toThrow(InvalidRange);
+        expect(execution).toThrow(InvalidRange);
     });
 
     it('should not accept a too small offset', () =>
     {
-        const result = () => validateRange(VALUES.RANGE.OFFSET_TOO_SMALL);
+        const execution = () => validateRange(VALUES.RANGE.OFFSET_TOO_SMALL);
 
-        expect(result).toThrow(InvalidRange);
+        expect(execution).toThrow(InvalidRange);
     });
 
     it('should accept valid lower bounds', () =>
