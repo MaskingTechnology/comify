a0ca193c29148de10be5f544f8dc113cceb5961e
Peter van Vliet <peter@masking.tech>
2024-06-22 10:19:08 +0200

    #284: second fixed version

--- src/webui/hooks/usePathParam.ts
+++ src/webui/hooks/usePathParam.ts
@@ -1,51 +1,45 @@
 
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 
 export function usePathParam(paramName: string, defaultValue?: string)
 {
     const navigate = useNavigate();
-    const params = useParams();
+    const { [paramName]: paramValue } = useParams();
     const { pathname } = useLocation();
     const [value, setValue] = useState<string | undefined>(undefined);
 
-    const getPathInfo = useCallback(() =>
+    const parameters = useMemo(() =>
     {
         const path = pathname.endsWith('/') ? pathname.slice(0, -1) : pathname;
-        const parts = path.split('/');
 
-        const paramValue = params[paramName];
-        const paramIndex = parts.indexOf(paramValue ?? '###');
+        return path.split('/');
 
-        return { parts, paramValue, paramIndex };
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [pathname]);
+
+    const paramIndex = useMemo(() =>
+    {
+        return parameters.indexOf(paramValue ?? '###');
+
+    }, [parameters, paramValue]);
 
     const createPath = useCallback(() =>
     {
         if (value === undefined) return;
 
-        const { parts, paramIndex, paramValue } = getPathInfo();
-
-        if (paramValue === value) return;
-
         paramIndex === -1
-            ? parts.push(value)
-            : parts[paramIndex] = value;
+            ? parameters.push(value)
+            : parameters[paramIndex] = value;
 
-        return parts.join('/');
+        return parameters.join('/');
 
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [value, getPathInfo]);
+    }, [value, parameters, paramIndex]);
 
     useEffect(() =>
     {
-        const { paramValue } = getPathInfo();
-
         setValue(paramValue ?? defaultValue);
 
-    }, [defaultValue, getPathInfo]);
+    }, [defaultValue, paramValue]);
 
     useEffect(() =>
     {
