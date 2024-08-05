746219b51a1ea75f1013ba8e0335e450a43646ff
John Meeuwissen <john@masking.tech>
2024-08-05 16:02:53 +0200

    #302 feat(web-ui): for you tab (#309)

--- src/webui/features/Timeline.tsx
+++ src/webui/features/Timeline.tsx
@@ -3,18 +3,18 @@ import { Ruler, Tab, Tabs } from '^/webui/designsystem';
 
 import { usePathParam } from '^/webui/hooks';
 
-import TimelineEverything from './TimelineEverything';
 import TimelineFollowing from './TimelineFollowing';
+import TimelineForYou from './TimelineForYou';
 
 export default function Feature()
 {
-    const [tab, setTab] = usePathParam('tab', 'everything');
+    const [tab, setTab] = usePathParam('tab', 'foryou');
 
     const separator = <Ruler direction='horizontal' size='small' />;
 
     return <Tabs selectedId={tab} onChange={setTab} separator={separator}>
-        <Tab id='everything' title='Everything'>
-            <TimelineEverything />
+        <Tab id='foryou' title='For you'>
+            <TimelineForYou />
         </Tab>
         <Tab id='following' title='Following'>
             <TimelineFollowing />
