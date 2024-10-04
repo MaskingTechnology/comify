c2bc279435a734438fabc8981421410e39bf73ce
Bas Meeuwissen <bas@masking.tech>
2024-10-05 00:33:04 +0200

    #333: updated to v0.8.2

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import jitar from '@jitar/plugin-vite';
+import jitar, { JitarConfig } from '@jitar/plugin-vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -7,27 +7,24 @@ const JITAR_URL = 'http://localhost:3000';
 const JITAR_SEGMENTS = [];
 const JITAR_MIDDLEWARES = ['./integrations/runtime/requesterMiddleware'];
 
+const jitarConfig: JitarConfig = {
+  sourceDir: 'src',
+  targetDir: 'dist',
+  jitarDir: 'domain',
+  jitarUrl: JITAR_URL,
+  segments: JITAR_SEGMENTS,
+  middleware: JITAR_MIDDLEWARES
+};
+
 export default defineConfig({
   publicDir: 'src/webui/public',
   build: {
     assetsDir: 'webui',
     emptyOutDir: false
   },
-  server: {
-    proxy: {
-      '/assets': JITAR_URL
-    }
-  },
   plugins: [
     react(),
     tsconfigPaths(),
-    jitar({
-      sourceDir: 'src',
-      targetDir: 'dist',
-      jitarDir: 'domain',
-      jitarUrl: 'http://localhost:3000',
-      segments: JITAR_SEGMENTS,
-      middleware: JITAR_MIDDLEWARES
-    })
+    jitar(jitarConfig)
   ]
 });
