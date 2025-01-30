d12a18934d53f663c4284b230411538da49e1142
Peter van Vliet <peter@masking.tech>
2025-01-30 12:14:37 +0100

    #373: added event broker as resource

--- src/integrations/runtime/setUpNode.ts
+++ src/integrations/runtime/setUpNode.ts
@@ -1,5 +1,6 @@
 
 import database from '^/integrations/database';
+import eventBroker from '^/integrations/eventbroker';
 import fileStore from '^/integrations/filestore';
 import notificationService from '^/integrations/notification';
 
@@ -7,6 +8,7 @@ try
 {
     await Promise.allSettled([
         database.connect(),
+        eventBroker.connect(),
         fileStore.connect(),
         notificationService.connect()
     ]);
@@ -16,6 +18,7 @@ catch (error: unknown)
     const disconnections = [];
 
     if (database.connected) disconnections.push(database.disconnect());
+    if (eventBroker.connected) disconnections.push(eventBroker.disconnect());
     if (fileStore.connected) disconnections.push(fileStore.disconnect());
     if (notificationService.connected) disconnections.push(notificationService.disconnect());
 
