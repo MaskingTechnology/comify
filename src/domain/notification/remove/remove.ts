91ed63c139b7bfb424f136b3a2ce3d3ce6a339fe
John Meeuwissen <john@masking.tech>
2025-03-24 10:13:43 +0100

    #342 fix sonarqube issue

--- src/domain/notification/remove/remove.ts
+++ src/domain/notification/remove/remove.ts
@@ -5,5 +5,5 @@ import { RECORD_TYPE } from '../definitions';
 
 export default async function remove(id: string): Promise<void>
 {
-    return database.updateRecord(RECORD_TYPE, id, { deleted: true }) as Promise<void>;
+    return database.updateRecord(RECORD_TYPE, id, { deleted: true });
 }
