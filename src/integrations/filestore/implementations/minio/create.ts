38b4a048e1f41514eeb19d7c9b110043521e2e70
Bas Meeuwissen <bas@masking.tech>
2025-07-29 13:02:40 +0200

    #429: update minio config to match env variables

--- src/integrations/filestore/implementations/minio/create.ts
+++ src/integrations/filestore/implementations/minio/create.ts
@@ -6,8 +6,8 @@ export default function create(): MinioFS
     const endPoint = process.env.MINIO_END_POINT ?? 'undefined';
     const port = Number(process.env.MINIO_PORT_NUMBER ?? 9000);
     const useSSL = process.env.MINIO_USE_SSL === 'true';
-    const accessKey = process.env.MINIO_ACCESS_KEY ?? 'undefined';
-    const secretKey = process.env.MINIO_SECRET_KEY ?? 'undefined';
+    const accessKey = process.env.MINIO_ROOT_USER ?? 'undefined';
+    const secretKey = process.env.MINIO_ROOT_PASSWORD ?? 'undefined';
 
     return new MinioFS({ endPoint, port, useSSL, accessKey, secretKey });
 }
