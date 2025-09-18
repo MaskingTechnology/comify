52b60aabd5a93391c24cfeb9727006b7af5a00b5
Peter van Vliet <peter@masking.tech>
2025-09-18 14:06:25 +0200

    #436: cleanup

--- src/integrations/runtime/tearDownWorker.ts
+++ src/integrations/runtime/tearDownWorker.ts
@@ -1,14 +1,10 @@
 
 import database from '^/integrations/database';
-import eventBroker from '^/integrations/eventbroker';
 import fileStore from '^/integrations/filestore';
-import notificationService from '^/integrations/notification';
 
 const disconnections = [];
 
 if (database.connected) disconnections.push(database.disconnect());
-if (eventBroker.connected) disconnections.push(eventBroker.disconnect());
 if (fileStore.connected) disconnections.push(fileStore.disconnect());
-if (notificationService.connected) disconnections.push(notificationService.disconnect());
 
 await Promise.allSettled(disconnections);
