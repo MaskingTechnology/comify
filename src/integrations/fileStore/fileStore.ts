14fbcebe2d6a0a53115935ab150b3acc053adbd3
Peter van Vliet <peter@masking.tech>
2026-01-16 10:57:49 +0100

    #452: updated dependencies

--- src/integrations/fileStore/fileStore.ts
+++ src/integrations/fileStore/fileStore.ts
@@ -8,18 +8,20 @@ function setUpMemory(): MemoryDriver
 
 function setUpMinio(): S3Driver
 {
-    const bucket = process.env.S3_BUCKET_NAME ?? '';
     const config = {
-        region: process.env.S3_REGION ?? 'local',
-        credentials: {
-            accessKeyId: process.env.S3_ROOT_USER ?? '',
-            secretAccessKey: process.env.S3_ROOT_PASSWORD ?? ''
-        },
-        forcePathStyle: true,
-        endpoint: process.env.S3_END_POINT ?? ''
+        bucketName:  process.env.S3_BUCKET_NAME ?? '',
+        clientConfig: {
+            region: process.env.S3_REGION ?? 'local',
+            credentials: {
+                accessKeyId: process.env.S3_ROOT_USER ?? '',
+                secretAccessKey: process.env.S3_ROOT_PASSWORD ?? ''
+            },
+            forcePathStyle: true,
+            endpoint: process.env.S3_END_POINT ?? ''
+        }
     };
 
-    return new S3Driver(config, bucket);
+    return new S3Driver(config);
 }
 
 export const driver = process.env.FILE_STORE_DRIVER === 's3'
