7ba99cd2284c92ff06b9aec2f1dd95191420d549
Bas Meeuwissen <bas@masking.tech>
2026-01-16 10:35:25 +0100

    #452: S3 implementation for filestore

--- src/integrations/fileStore/fileStore.ts
+++ src/integrations/fileStore/fileStore.ts
@@ -1,23 +1,28 @@
 
-import FileStore, { MinioDriver, MemoryDriver } from '@theshelf/filestore';
+import FileStore, { MemoryDriver, S3Driver } from '@theshelf/filestore';
 
 function setUpMemory(): MemoryDriver
 {
     return new MemoryDriver();
 }
 
-function setUpMinio(): MinioDriver
+function setUpMinio(): S3Driver
 {
-    return new MinioDriver({
-        endPoint: process.env.MINIO_END_POINT ?? '',
-        port: Number(process.env.MINIO_PORT_NUMBER ?? 9000),
-        useSSL: process.env.MINIO_USE_SSL === 'true',
-        accessKey: process.env.MINIO_ROOT_USER ?? '',
-        secretKey: process.env.MINIO_ROOT_PASSWORD ?? ''
-    });
+    const bucket = process.env.S3_BUCKET_NAME ?? '';
+    const config = {
+        region: process.env.S3_REGION ?? 'local',
+        credentials: {
+            accessKeyId: process.env.S3_ROOT_USER ?? '',
+            secretAccessKey: process.env.S3_ROOT_PASSWORD ?? ''
+        },
+        forcePathStyle: true,
+        endpoint: process.env.S3_END_POINT ?? ''
+    };
+
+    return new S3Driver(config, bucket);
 }
 
-export const driver = process.env.FILE_STORE_DRIVER === 'minio'
+export const driver = process.env.FILE_STORE_DRIVER === 's3'
     ? setUpMinio()
     : setUpMemory();
 
