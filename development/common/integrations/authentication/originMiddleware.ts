986ea2d179d1df5a74ab8ed2254ad1b7aa5d2606
Bas Meeuwissen <bas@masking.tech>
2026-05-21 22:13:09 +0300

    #488: updated ci config

--- development/common/integrations/authentication/originMiddleware.ts
+++ development/common/integrations/authentication/originMiddleware.ts
@@ -4,6 +4,6 @@ import { OriginMiddleware } from '@jitar-plugins/http';
 const options = {
     sameSite: process.env.ORIGIN_COOKIE_SAME_SITE || 'Strict',
     secure: process.env.ORIGIN_COOKIE_SECURE === 'true'
-}
+};
 
 export default new OriginMiddleware(options);
