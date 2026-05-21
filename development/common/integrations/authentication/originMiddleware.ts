e468152525fe6645e1391e705d52ce02f3bc77fe
Bas Meeuwissen <bas@masking.tech>
2026-05-21 22:35:01 +0300

    #488: processed feedback

--- development/common/integrations/authentication/originMiddleware.ts
+++ development/common/integrations/authentication/originMiddleware.ts
@@ -2,7 +2,7 @@
 import { OriginMiddleware } from '@jitar-plugins/http';
 
 const options = {
-    sameSite: process.env.ORIGIN_COOKIE_SAME_SITE || 'Strict',
+    sameSite: process.env.ORIGIN_COOKIE_SAME_SITE ?? 'Strict',
     secure: process.env.ORIGIN_COOKIE_SECURE === 'true'
 };
 
