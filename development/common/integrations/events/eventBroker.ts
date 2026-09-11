886034094303e8c7d2c2da79f915e6ca93eb06ad
Peter van Vliet <peter@masking.tech>
2026-09-11 15:46:30 +0200

    #496: added kafka driver

--- development/common/integrations/events/eventBroker.ts
+++ development/common/integrations/events/eventBroker.ts
@@ -1,10 +1,27 @@
 
 import ConnectionManager from '@theshelf/connection';
 import EventBroker, { MemoryDriver } from '@theshelf/events';
+import { KafkaDriver } from '@theshelf/events-driver-kafka';
 
 import { shelfLogger } from '^/integrations/logging';
 
-export const driver = new MemoryDriver();
+function setUpMemory(): MemoryDriver
+{
+    return new MemoryDriver();
+}
+
+function setUpKafka(): KafkaDriver
+{
+    return new KafkaDriver({
+        brokers: [process.env.KAFKA_BROKER ?? ''],
+        groupId: process.env.KAFKA_GROUP_ID ?? '',
+        clientId: process.env.KAFKA_CLIENT_ID ?? ''
+    });
+}
+
+export const driver = process.env.EVENT_BROKER_DRIVER === 'kafka'
+    ? setUpKafka()
+    : setUpMemory();
 
 const eventBroker = new EventBroker(driver, shelfLogger);
 
